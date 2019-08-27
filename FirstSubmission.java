dbee1a382001db08420fd62fa07df7326d850322
agokhale22 <43002655+agokhale22@users.noreply.github.com>
2019-08-27 17:38:20 -0400

    Add files via upload

---
+++ FirstSubmission.java
@@ -0,0 +1,14 @@
+
+public class FirstSubmission 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.println(" ////////");
+	System.out.println(" d o o b");
+	System.out.println(" | ^   |");
+	System.out.println(" | --- |");
+	System.out.println("+-------+");
+	}
+
+}
